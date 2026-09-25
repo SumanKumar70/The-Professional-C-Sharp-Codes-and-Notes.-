5a4d4501e2e30c8dcf19c4bc98aa62a9d966c7c6
Suman Kumar Hui <sumankumarhui2002@gmail.com>
2026-09-25 17:06:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Student
+{
+    static void Main(string[] args)
+    {
+        
+        string name = "Rahul";
+        int age = 20;
+        double marks = 85.5;
+        char grade = 'A';
+
+        
+        Console.WriteLine("Student Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Marks: " + marks);
+        Console.WriteLine("Grade: " + grade);
+    }
+}
