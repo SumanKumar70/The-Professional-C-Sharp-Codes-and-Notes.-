3144dde44db9b2fa342e5d47046d5751c7385970
Suman Kumar Hui <sumankumarhui2002@gmail.com>
2026-09-25 17:10:24 +0530

    Add files via upload

---
+++ Scope Of Variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int mainNumber = 10;
+
+        Console.WriteLine("Main variable: " + mainNumber);
+
+        
+        {
+           
+            int blockNumber = 20;
+
+            Console.WriteLine("Main variable inside block: " + mainNumber);
+            Console.WriteLine("Block variable: " + blockNumber);
+        }
+      
+        Console.WriteLine("Main variable after block: " + mainNumber);
+
+    
+    }
+}
