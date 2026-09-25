8e5e32adfe8c844e43ff04fe811d40c4d1aa820e
Suman Kumar Hui <sumankumarhui2002@gmail.com>
2026-09-25 17:09:20 +0530

    Add files via upload

---
+++ Data Types.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        
+        int age = 20;
+        double height = 5.8;
+        char grade = 'A';
+        bool isStudent = true;
+        string name = "Rahul";
+        decimal fees = 25000.50m;
+
+        
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Height: " + height);
+        Console.WriteLine("Grade: " + grade);
+        Console.WriteLine("Is Student: " + isStudent);
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Fees: " + fees);
+    }
+}
