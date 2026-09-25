3bbc0484eb1f18b3f3aae6b8271f4997a99ed21a
Suman Kumar Hui <sumankumarhui2002@gmail.com>
2026-09-25 17:29:48 +0530

    Add files via upload

---
+++ 14.Method Overloading.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+   
+    static int Add(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+   
+    static int Add(int number1, int number2, int number3)
+    {
+        return number1 + number2 + number3;
+    }
+
+   
+    static double Add(double number1, double number2)
+    {
+        return number1 + number2;
+    }
+
+    static void Main()
+    {
+        int result1 = Add(10, 20);
+        int result2 = Add(10, 20, 30);
+        double result3 = Add(10.5, 20.5);
+
+        Console.WriteLine("Add(int, int): " + result1);
+        Console.WriteLine("Add(int, int, int): " + result2);
+        Console.WriteLine("Add(double, double): " + result3);
+    }
+}
---
+++ 15.Constructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Student
+{
+    
+    public string Name;
+    public int Age;
+
+    
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Student student = new Student("Rahul", 20);
+
+       
+        Console.WriteLine("Name: " + student.Name);
+        Console.WriteLine("Age: " + student.Age);
+    }
+}
